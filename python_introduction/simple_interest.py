80ce77262995cfb84a651711f158f1096886b853
Eshetu-Ab <eshetu190@gmail.com>
2024-06-23 13:55:14 +0300

    simple_interest.py added

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,9 @@
+# Define variables
+principal = 1000
+rate = 0.05
+time = 3
+
+# Calculate the simple interest
+interest = principal * rate * time
+# Print the interest in the specified format
+print(f"The simple interest is: {interest}")
